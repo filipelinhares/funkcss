a8dc8beb8347e4520de9e989f9287f2d84b84a27
Gustavo Guichard <gustavoguichard@gmail.com>
2016-03-21 10:18:47 -0300

    Generate selectors commented by property

--- scripts/writers.js
+++ scripts/writers.js
@@ -3,6 +3,29 @@ const fs = require('fs')
 const whiteList = require('./white-list')
 const reduce = require('./utils').reduce
 
+const getFirstValue = obj => obj[Object.keys(obj)[0]] || ''
+
+const generateFullClasses = object => {
+  return generateCSSString(object, 'EMMET SELECTORS')
+}
+
+const generateFilteredClasses = object => {
+  const searchMatch = (match, obj) => {
+    return reduce((final, value, key) => {
+      if(value.indexOf(match) === 0) final[key] = value
+      return final
+    }, {}, obj)
+  }
+  const properties = whiteList.map(prop => searchMatch(prop, object))
+  return reduce((final, obj) => {
+    const firstValue = getFirstValue(obj)
+    const comment = firstValue.split(':')[0].toUpperCase()
+    return `${final}
+${generateCSSString(obj, comment)}
+`
+  }, '', properties)
+}
+
 const generateCSSString = (object, comment) => {
   const initial = `/*
 
@@ -16,27 +39,13 @@ const generateCSSString = (object, comment) => {
   ), initial, object)
 }
 
-const filterStyles = object => {
-  const searchMatch = (match, obj) => {
-    return reduce((final, value, key) => {
-      if(value.indexOf(match) === 0) final[key] = value
-      return final
-    }, {}, obj)
-  }
-  const properties = whiteList.map(prop => searchMatch(prop, object))
-  var finalObject = {}
-  properties.forEach(prop => Object.assign(finalObject, prop))
-  return finalObject
-}
-
 module.exports = object => {
   const FILE_DIR = path.join(__dirname, "../lib")
   const FULL_FILE_NAME = '_emmet-selectors.css'
   const COMPACT_FILE_NAME = '_general-selectors.css'
-  const filteredStyles = filterStyles(object)
 
-  const fullCss = generateCSSString(object, 'EMMET SELECTORS')
-  const compactCss = generateCSSString(filteredStyles, 'GENERAL SELECTORS')
+  const fullCss = generateFullClasses(object)
+  const compactCss = generateFilteredClasses(object)
 
   fs.writeFileSync([FILE_DIR, FULL_FILE_NAME].join('/'), fullCss);
   fs.writeFileSync([FILE_DIR, COMPACT_FILE_NAME].join('/'), compactCss);
